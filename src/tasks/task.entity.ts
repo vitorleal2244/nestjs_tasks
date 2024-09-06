e59396514cba5e99b8f591fa0bf9e32981f373b3
Vitor Leal <39217722+vitorleal2244@users.noreply.github.com>
2024-09-06 09:05:04 +0100

    [NestJS][Tasks-Management] - Added restrictions with logged in user

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { User } from 'src/auth/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
 @Entity()
@@ -14,4 +16,10 @@ export class Task {
 
     @Column()
     status: TaskStatus;
+
+    @ManyToOne(_type => User, user => user.tasks, { eager: false })
+    @Exclude({
+        toPlainOnly: true
+    })
+    user: User
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task } from './task.entity';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -15,17 +16,19 @@ export class TasksService {
         private tasksRepository: MongoRepository<Task>,
     ) { }
 
-    async getAllTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    async getAllTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
         const { status, search } = filterDto;
 
         const query = this.tasksRepository.createQueryBuilder('task');
+
+        query.where({ user });
 
         if (status) {
             query.andWhere('task.status = :status', { status: 'OPEN' });
         }
 
         if (search) {
-            query.andWhere('lOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+            query.andWhere('(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
                 { search: `%${search}%` })
         }
 
@@ -33,8 +36,8 @@ export class TasksService {
         return tasks;
     }
 
-    async getTaskById(id: string): Promise<Task> {
-        const found = await this.tasksRepository.findOne({ where: { id } });
+    async getTaskById(id: string, user: User): Promise<Task> {
+        const found = await this.tasksRepository.findOne({ where: { id, user } });
 
         if (!found) {
             throw new NotFoundException(`Task with ID ${id} not found!`);
@@ -43,13 +46,14 @@ export class TasksService {
         return found;
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
         const { title, description } = createTaskDto;
 
         const task = this.tasksRepository.create({
             title,
             description,
-            status: TaskStatus.OPEN
+            status: TaskStatus.OPEN,
+            user
         });
 
         await this.tasksRepository.save(task);
@@ -57,8 +61,8 @@ export class TasksService {
         return task;
     }
 
-    async deleteTask(id: string): Promise<boolean> {
-        const result = await this.tasksRepository.delete(id);
+    async deleteTask(id: string, user: User): Promise<boolean> {
+        const result = await this.tasksRepository.delete({ id, user });
 
         if (result.affected === 0) {
             throw new NotFoundException(`Task with ID ${id} not found!`);
@@ -67,8 +71,8 @@ export class TasksService {
         return true;
     }
 
-    async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-        const task = await this.getTaskById(id);
+    async updateTaskStatus(id: string, status: TaskStatus, user: User): Promise<Task> {
+        const task = await this.getTaskById(id, user);
 
         task.status = status;
         await this.tasksRepository.save(task);
